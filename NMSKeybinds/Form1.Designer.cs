eaa16b4dbc8fea3afbe77970925691488673ec9a
Les Ferguson <lhf@xtra.co.nz>
2022-07-30 15:55:26 +1200

    Add project files.

---
+++ NMSKeybinds/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace NMSKeybinds
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.LVSettings = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.ComboFilterSet = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxFilterButton = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxFilterAction = new System.Windows.Forms.TextBox();
+            this.BtnRefresh = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.viewNMSSettingsDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(494, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.viewNMSSettingsDirectoryToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Enabled = false;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(221, 6);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(224, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // LVSettings
+            // 
+            this.LVSettings.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LVSettings.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.LVSettings.FullRowSelect = true;
+            this.LVSettings.GridLines = true;
+            this.LVSettings.HideSelection = false;
+            this.LVSettings.Location = new System.Drawing.Point(12, 123);
+            this.LVSettings.Name = "LVSettings";
+            this.LVSettings.Size = new System.Drawing.Size(470, 682);
+            this.LVSettings.TabIndex = 9;
+            this.LVSettings.UseCompatibleStateImageBehavior = false;
+            this.LVSettings.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ActionSet";
+            this.columnHeader1.Width = 160;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Action";
+            this.columnHeader2.Width = 160;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Button";
+            this.columnHeader3.Width = 120;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.BtnClear);
+            this.panel1.Controls.Add(this.ComboFilterSet);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.TxFilterButton);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.TxFilterAction);
+            this.panel1.Controls.Add(this.BtnRefresh);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(470, 90);
+            this.panel1.TabIndex = 1;
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnClear.Location = new System.Drawing.Point(375, 4);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(92, 23);
+            this.BtnClear.TabIndex = 9;
+            this.BtnClear.Text = "Clear Filters";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // ComboFilterSet
+            // 
+            this.ComboFilterSet.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ComboFilterSet.FormattingEnabled = true;
+            this.ComboFilterSet.Location = new System.Drawing.Point(58, 6);
+            this.ComboFilterSet.Name = "ComboFilterSet";
+            this.ComboFilterSet.Size = new System.Drawing.Size(296, 21);
+            this.ComboFilterSet.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Set";
+            // 
+            // TxFilterButton
+            // 
+            this.TxFilterButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TxFilterButton.Location = new System.Drawing.Point(58, 59);
+            this.TxFilterButton.Name = "TxFilterButton";
+            this.TxFilterButton.Size = new System.Drawing.Size(296, 20);
+            this.TxFilterButton.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Button";
+            // 
+            // TxFilterAction
+            // 
+            this.TxFilterAction.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TxFilterAction.Location = new System.Drawing.Point(58, 33);
+            this.TxFilterAction.Name = "TxFilterAction";
+            this.TxFilterAction.Size = new System.Drawing.Size(296, 20);
+            this.TxFilterAction.TabIndex = 5;
+            // 
+            // BtnRefresh
+            // 
+            this.BtnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnRefresh.Location = new System.Drawing.Point(375, 56);
+            this.BtnRefresh.Name = "BtnRefresh";
+            this.BtnRefresh.Size = new System.Drawing.Size(92, 25);
+            this.BtnRefresh.TabIndex = 8;
+            this.BtnRefresh.Text = "Apply Filters";
+            this.BtnRefresh.UseVisualStyleBackColor = true;
+            this.BtnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Action";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(221, 6);
+            // 
+            // viewNMSSettingsDirectoryToolStripMenuItem
+            // 
+            this.viewNMSSettingsDirectoryToolStripMenuItem.Enabled = false;
+            this.viewNMSSettingsDirectoryToolStripMenuItem.Name = "viewNMSSettingsDirectoryToolStripMenuItem";
+            this.viewNMSSettingsDirectoryToolStripMenuItem.Size = new System.Drawing.Size(224, 22);
+            this.viewNMSSettingsDirectoryToolStripMenuItem.Text = "View NMS Settings Directory";
+            this.viewNMSSettingsDirectoryToolStripMenuItem.Click += new System.EventHandler(this.viewNMSSettingsDirectoryToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 817);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.LVSettings);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(420, 180);
+            this.Name = "MainForm";
+            this.Text = "NMS Keybindings View";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ListView LVSettings;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox TxFilterAction;
+        private System.Windows.Forms.Button BtnRefresh;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox ComboFilterSet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxFilterButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.ToolStripMenuItem viewNMSSettingsDirectoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+    }
+}
+
---
+++ NMSKeybinds/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using NMSKeybinds.Properties;
+using System.Diagnostics;
+
+namespace NMSKeybinds
+{
+    /// <summary>
+    /// Various filtering and sorting features for reviewing:
+    /// ...Program Files...\Steam\steamapps\common\No Man's Sky\Binaries\SETTINGS\TKGAMESETTINGS.MXML
+    /// 
+    /// Reason: I have a lifetime habit of using ESDF instead of WASD in games.
+    /// When I started remapping NMS key bindings there were some sub-menu options which cannot
+    /// be changed in game settings. This is a tool to enable assessment and planning alternative
+    /// layouts. Will decide later if it is wise to include rewriting the config file.
+    /// 
+    /// Currently: E:\Programs_x86\Steam\steamapps\common\No Man's Sky\Binaries\SETTINGS
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        private string mCurrentFilePath;
+        private XmlDocument mCurrentSettings;
+        private List<NMSKeyBinding> mBindings;
+        private string mLastNMSSettingsDir;
+
+        struct NMSKeyBinding 
+        { 
+            public string ActionSet; 
+            public string Action;
+            public string Button;
+            public ListViewItem ToListViewItem()
+                { return new ListViewItem(new string[] { this.ActionSet, this.Action, this.Button }); }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            /* Check 'last NSM settings dir' in user settings.
+             * If preset & valid, enable menu option for viewing the directory.
+             */
+            mLastNMSSettingsDir = Settings.Default.NMSSettingsDirectory;
+            viewNMSSettingsDirectoryToolStripMenuItem.Enabled = !string.IsNullOrEmpty(mLastNMSSettingsDir) && Directory.Exists(mLastNMSSettingsDir);
+        }
+
+        #region EventHandlers
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SelectSettingsFile();
+        }
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveSettingsFile("");
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            if (ComboFilterSet.Items.Count > 0)
+                ComboFilterSet.SelectedIndex = 0;
+            TxFilterAction.Text = "";
+            TxFilterButton.Text = "";
+            RefreshList();
+        }
+
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshList();
+        }
+
+        private void viewNMSSettingsDirectoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(mLastNMSSettingsDir) && Directory.Exists(mLastNMSSettingsDir))
+            {
+                // TODO shell-execute directory and let the system default handle it
+                Process ViewProcess = new Process();
+                try
+                {
+                    ViewProcess.StartInfo.UseShellExecute = true;
+                    ViewProcess.StartInfo.FileName = mLastNMSSettingsDir;
+                    ViewProcess.StartInfo.WorkingDirectory = mLastNMSSettingsDir;
+                    ViewProcess.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+                    ViewProcess.StartInfo.CreateNoWindow = true;
+                    ViewProcess.Start();
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(this, $"Process creation failed\r\n{exc.Message}", "Directory View Failed");
+                }
+            }
+        }
+        #endregion
+
+        private void SelectSettingsFile()
+        {
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.AddExtension = true;
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.Filter = "MXML Files|*.MXML|All Files|*.*";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.DefaultExt = ".MXML";
+            openFileDialog1.Title = "TKGAMESETTINGS Key Bindings";
+
+            if (Settings.Default.NMSSettingsDirectory.Length > 0 && Directory.Exists(Settings.Default.NMSSettingsDirectory))
+                openFileDialog1.InitialDirectory = Settings.Default.NMSSettingsDirectory;
+            else
+                openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string getFile = openFileDialog1.FileName;
+                if (File.Exists(getFile))
+                    LoadSettingsFile(getFile);
+            }
+        }
+
+        /// <summary>
+        /// Open the settings XML file.
+        /// Pull the bits we are interested in into a data collection.
+        /// TODO manage references between internal data collection and original
+        /// XML elements, for updating the XML with changes (if implemented).
+        /// </summary>
+        /// <param name="getFile"></param>
+        private void LoadSettingsFile(string getFile)
+        {
+            mCurrentSettings = new XmlDocument();
+            viewNMSSettingsDirectoryToolStripMenuItem.Enabled = false;
+            try
+            {
+                /* Save directory to users 'last NMS settings directory' config value
+                 */
+                FileInfo fileInfo = new FileInfo(getFile);
+                DirectoryInfo dir = fileInfo.Directory;
+                if (dir.Exists)
+                {
+                    Settings.Default.NMSSettingsDirectory = dir.FullName;
+                    Settings.Default.Save();
+                    viewNMSSettingsDirectoryToolStripMenuItem.Enabled = true;
+                }
+                mCurrentSettings.Load(getFile);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong there\r\n" + ex.Message, "We had a small problem opening that file");
+                return;
+            }
+
+            LVSettings.Items.Clear();
+            ComboFilterSet.Items.Clear();
+            ComboFilterSet.Items.Add("All action sets");
+            mBindings = new List<NMSKeyBinding>();
+            XmlNodeList level1Props = mCurrentSettings.SelectNodes("/Data/Property");
+            XmlNode keyMapping = null;
+            string propName = "";
+            /* Not sure why my config has an empty "KeyMapping" node, and a populated "KeyMapping2" node.
+             * 	<Property name="KeyMapping" />
+             * 	<Property name="KeyMapping2">
+             * 	    <Property name="KeyMapping2" value="GcInputActionMapping2.xml">
+             *          <Property name="ActionSet" value="FRONTEND" />
+             *          <Property name="Action" value="BaseBuilding_ToggleWiring" />
+             *          <Property name="Button" value="KeyQ" />
+             *          <Property name="Axis" value="None" />
+             *      </Property>
+             *      ...etc...
+             *  </Property>
+             *  
+             * Look for first 'KeyMap...' node which has child nodes, assume that is the main config set.
+             */
+            foreach (XmlNode level1Prop in level1Props)
+            {
+                propName = level1Prop.Attributes["name"].Value;
+                if (propName.StartsWith("KeyMap") && level1Prop.ChildNodes.Count > 0)
+                {
+                    keyMapping = level1Prop;
+                    break;
+                }
+            }
+
+            if (keyMapping != null && keyMapping.ChildNodes.Count > 0)
+            {
+                /* TODO save path to keyMapping node, so the full xpath to each of the key-binding
+                 * properties in the collection can be referenced later, when updates need to be
+                 * made to the original XML document.  Maybe formulate the xpath and add it to the
+                 * NMSKeyBinding struct, so each property has its own reference for updating the XML.
+                 */
+                List<string> allActionSets = new List<string>();
+                foreach (XmlNode level2Prop in keyMapping.ChildNodes)
+                {
+                    NMSKeyBinding keyBinding = new NMSKeyBinding();
+                    XmlAttribute attr;
+                    XmlNode nodeValue = level2Prop.SelectSingleNode("Property[@name='ActionSet']");
+                    if (nodeValue != null)
+                    {
+                        attr = nodeValue.Attributes["value"];
+                        if (attr != null)
+                        { 
+                            keyBinding.ActionSet = attr.Value;
+                            if (!allActionSets.Contains(attr.Value))
+                            {
+                                // Add the ActionSet to the dropdown list, for filtering the listview
+                                allActionSets.Add(attr.Value);
+                                ComboFilterSet.Items.Add(attr.Value);
+                            }
+                        }
+                    }
+                    nodeValue = level2Prop.SelectSingleNode("Property[@name='Action']");
+                    if (nodeValue != null)
+                    {
+                        attr = nodeValue.Attributes["value"];
+                        if (attr != null)
+                            keyBinding.Action = attr.Value;
+
+                    }
+                    nodeValue = level2Prop.SelectSingleNode("Property[@name='Button']");
+                    if (nodeValue != null)
+                    {
+                        attr = nodeValue.Attributes["value"];
+                        if (attr != null)
+                            keyBinding.Button = attr.Value;
+                    }
+
+                    if (keyBinding.Button.Length > 0)
+                        mBindings.Add(keyBinding);
+               }
+            }
+            ComboFilterSet.SelectedIndex = 0;
+            RefreshList();
+        }
+
+        private void SaveSettingsFile(string newFileName = "")
+        {
+            /* TODO
+             * - zip current file to a timestamped backup
+             * - overwrite active config with updated XML
+             * - think about displaying existing backup copies and providing a quick-swap feature for reverting to any previous save
+             */
+            MessageBox.Show("Not implemented yet", "Save Config Changes", MessageBoxButtons.OK);
+        }
+
+        /// <summary>
+        /// Reload the listview from the stored config data, applying the current filters
+        /// </summary>
+        private void RefreshList()
+        {
+            LVSettings.Items.Clear();
+            LVSettings.SuspendLayout();
+            string filterAction = TxFilterAction.Text.Trim();
+            string filterButton = TxFilterButton.Text.Trim();
+            string filterSet = "";
+            if (ComboFilterSet.SelectedIndex > 0)
+                filterSet = ComboFilterSet.SelectedItem.ToString();
+
+            foreach (NMSKeyBinding keyBinding in mBindings)
+            {
+                if (filterSet.Length > 0 && !keyBinding.ActionSet.Equals(filterSet, StringComparison.OrdinalIgnoreCase))
+                    continue;
+                if (filterAction.Length > 0 && keyBinding.Action.IndexOf(filterAction, StringComparison.OrdinalIgnoreCase) < 0)
+                    continue;
+                if (filterButton.Length > 0 && keyBinding.Button.IndexOf(filterButton, StringComparison.OrdinalIgnoreCase) < 0)
+                    continue;
+
+                LVSettings.Items.Add(keyBinding.ToListViewItem());
+            }
+            LVSettings.ResumeLayout();
+        }
+
+    }
+}
